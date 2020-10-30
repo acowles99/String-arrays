bbe7dee9bc3fc2fda341538022c18f2ab4b739ab
Alexis Cowles <alexis.cowles@patelhs.org>
2020-10-30 13:11:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    
+    String [] food = {"Pasta", "Tacos", "Burgers", "Cake", "Sandwich", "Pizza", "Chips"};
+
+    String index = "Pie";
+    for (int i = 0; i < food.length - 1; i++)
+    {
+      System.out.println( food[i] );
+     }
+    
+    System.out.println(index);
+
+
+  }
+}
